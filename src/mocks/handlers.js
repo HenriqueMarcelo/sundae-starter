6f8ddf745fa2fe66495ce2649a38673af2231788
Marcelo Henrique Fonseca <marcelohenriquedasilvafonseca@gmail.com>
2024-09-08 17:13:13 -0300

    feat: create my test

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -3,7 +3,6 @@ import { http, HttpResponse } from "msw";
 
 export const handlers = [
   http.get("http://localhost:3030/scoops", () => {
-    // ...and respond to them using this JSON response.
     return HttpResponse.json([
       {
         name: "Chocolate",
@@ -12,6 +11,23 @@ export const handlers = [
       {
         name: "Vanilla",
         imagePath: "/image/vanilla.png",
+      },
+    ]);
+  }),
+
+  http.get("http://localhost:3030/toppings", () => {
+    return HttpResponse.json([
+      {
+        name: "Cherries",
+        imagePath: "/images/cherries.png",
+      },
+      {
+        name: "M&Ms",
+        imagePath: "/images/m-and-ms.png",
+      },
+      {
+        name: "Hot fudge",
+        imagePath: "/images/hot-fudge.png",
       },
     ]);
   }),
