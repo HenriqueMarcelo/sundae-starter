dcf21075021052a50f56eef6c98b29a8b9d07a78
Marcelo Henrique Fonseca <marcelohenriquedasilvafonseca@gmail.com>
2024-09-13 14:11:07 -0300

    style: fix code style

--- src/pages/entry/test/OrderEntry.test.jsx
+++ src/pages/entry/test/OrderEntry.test.jsx
@@ -1,24 +1,22 @@
-import { render, screen } from "@testing-library/react";
-import { OrderEntry } from "../OrderEntry";
-import { http, HttpResponse } from "msw";
-import { server } from "../../../mocks/server";
-import { expect } from "vitest";
+import { render, screen } from '@testing-library/react'
+import { OrderEntry } from '../OrderEntry'
+import { http, HttpResponse } from 'msw'
+import { server } from '../../../mocks/server'
+import { expect } from 'vitest'
 
-test("handles error for scoops and toppings routes", async () => {
+test('handles error for scoops and toppings routes', async () => {
   server.resetHandlers(
-    http.get("http://localhost:3030/scoops", () => {
-      return new HttpResponse(null, { status: 500 });
+    http.get('http://localhost:3030/scoops', () => {
+      return new HttpResponse(null, { status: 500 })
     }),
-    http.get("http://localhost:3030/toppings", () => {
-      return new HttpResponse(null, { status: 500 });
+    http.get('http://localhost:3030/toppings', () => {
+      return new HttpResponse(null, { status: 500 })
     })
-  );
+  )
 
-  render(<OrderEntry />);
+  render(<OrderEntry />)
 
-  const alerts = await screen.findAllByRole("alert", {
-    name: "An unexpected error occurred. Please try again later.",
-  });
+  const alerts = await screen.findAllByText('An unexpected error occurred. Please try again later.')
 
-  expect(alerts).toHaveLength(2);
-});
+  expect(alerts).toHaveLength(2)
+})
