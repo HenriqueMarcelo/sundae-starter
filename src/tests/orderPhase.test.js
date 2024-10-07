b26824b04cf0f6bd4a18757fa0dfb0efceb4965a
Marcelo Henrique Fonseca <marcelohenriquedasilvafonseca@gmail.com>
2024-10-07 16:26:34 -0300

    prepare for final test

---
+++ src/tests/orderPhase.test.js
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react'
+import App from '../App'
+
+test('order phases for happy path', () => {
+  // render the app
+  render(<App />)
+
+  // add ice cream scoops and toppings
+
+  // find and click order button
+
+  // check if summary information is correct base on order
+
+  // accept terms and conditions and click button to confirm order
+
+  // confirm order number on confirmation page
+
+  // click "new order" button on confirmation page
+
+  // check that scoops and toppings subtotal have been reset
+})
